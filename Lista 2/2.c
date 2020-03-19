3eff6f48718db5bbc9ee7a4acdec8611750f74e1
Reinan de Souza <reinandesouza01@gmail.com>
2020-03-19 17:31:25 -0300

    Criando lista duplamente encadeada circular

---
+++ Lista 2/2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct no{
+
+	int conteudo;
+	struct no *anterior;
+	struct no *proximo;
+};
+
+typedef struct no No;
+
+No *criarLista(){
+
+	No *inicio = (No*)malloc(sizeof(No));
+
+	if (inicio == NULL)
+		return NULL;
+
+	inicio->anterior = NULL;
+	inicio->proximo = inicio;
+
+	return inicio;
+}
+
+
+void main(){
+
+
+}
