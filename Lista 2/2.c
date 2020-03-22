be9cee1718cd984701153b41273c48baf5583e1b
Reinan de Souza <reinandesouza01@gmail.com>
2020-03-22 04:38:12 -0300

    Inserir no final

--- Lista 2/2.c
+++ Lista 2/2.c
@@ -1,112 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct no{
-
+struct node{
+	
 	int conteudo;
-	struct no *anterior;
-	struct no *proximo;
+	struct node *anterior;
+	struct node *proximo;
 };
 
-typedef struct no No;
+typedef struct node Node;
 
-//criacao de uma lista com cabeca
-No *criar(){
 
-	No *inicio = (No*)malloc(sizeof(No));
+// funcao de inserir no final
+int inserirFim(No** inicio, int valor){
 
-	if (inicio == NULL)
-		return NULL;
-
-	inicio->anterior = NULL;
-	inicio->proximo = NULL;
-
-	return inicio;
-}
-
-int inserirInicio(No **inicio, int valor){
-
-//inicio == pego o endereco que está armazenado em inicio
-//*inicio == pego o endereco de inicio
-//**inicio == pego o valor para onde inicio está apontando
-
-	if (inicio == NULL) // não existe lista
+	if (inicio == NULL) // lista nao existe
 		return 0;
 
-	No *novo = (No*)malloc(sizeof(No));
-
+	Node *novo = (Node*)malloc(sizeof(No));
+	
 	if (novo == NULL)
 		return 0;
 
 	novo->conteudo = valor;
-
-	if ((*inicio)->proximo == NULL){ // lista está vazia
-		
-		novo->anterior = (*inicio)->proximo;
-		novo->proximo = novo;
-		
-		(*inicio)->proximo = novo;
-		
 	
-	}else{
+	//se lista esta vazia, crie um unico nó
+	if (*inicio == NULL){
 
-		No *aux = (*inicio)->proximo;
+		novo->anterior = novo;
+		novo->proximo = novo;
 
-		while(aux->proximo != (*inicio)->proximo)
-			aux = aux->proximo;
+		*inicio = novo;
 
-
-		aux->proximo = novo;
-		novo->anterior = aux;
-		novo->proximo = (*inicio)->proximo;
-
-		(*inicio)->proximo = novo;
-
+		return 1;
 	}
+
+	// if lista nao estiver vazia
+	
+	// encontre o ultimo nó
+	Node *ultimo = (*inicio)->anterior;
+
+	// fazendo com que o proximo de novo, recebe o atual inicio
+	novo->proximo = *inicio;
+
+	// anterior do inicio, recebe novo
+	(*inicio)->anterior = novo;
+
+	// fazendo de novo anterior, o antigo ultimo
+	novo->anterior = ultimo;
+
+	// fazendo com que proximo do novo, se torne o antigo ultimo
+	ultimo->proximo = novo;
 
 	return 1;
 }
 
-
-void imprimeListaOrdemDireta(No **lista){
-
-	if (lista == NULL || (*lista)->proximo == NULL)
-		return;
-
-	No *atual = (*lista)->proximo;
-
-	do{
-		printf("-> %d ", atual->conteudo);
-		atual = atual->proximo;
-	
-	}while (atual != (*lista)->proximo);
-}
-
-void imprimeListaOrdemInversa(No **lista){
-
-	if (lista == NULL || (*lista)->proximo == NULL)
-		return;
-
-	No *ultimo = (*lista)->proximo;
-
-	while (ultimo->proximo != (*lista)->proximo)
-		ultimo = ultimo->proximo;
-
-	while(ultimo != (*lista)->proximo){
-		printf(" ->%d ", ultimo->conteudo);
-		ultimo = ultimo->anterior; 
-	}
-}
-
-void main(){
-
-	No *inicio = criar();
-
-	inserirInicio(&inicio, 3);
-	inserirInicio(&inicio,1);
-	inserirInicio(&inicio,5);
-	imprimeListaOrdemDireta(&inicio);
-	imprimeListaOrdemInversa(&inicio);	
-
-}
 
