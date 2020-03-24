59ca340587a5bb3c33c259f51d5114f053eba46e
Reinan de Souza <reinandesouza01@gmail.com>
2020-03-23 22:43:17 -0300

    Questao 6

--- Lista 2/6.c
+++ Lista 2/6.c
@@ -21,11 +21,11 @@ struct node{
 	struct node *proximo;
 };
 
-typedef node Node;
+typedef struct node Node;
 
 Node *criarPilha(){
 
-	Node *pilha = (Node*)malloc(sizeof);
+	Node *pilha = (Node*)malloc(sizeof(Node));
 
 	if (pilha == NULL)
 		return NULL;
@@ -43,7 +43,8 @@ int vazia(Node *pilha){
 	return 0;
 }
 
-int cheia(Node **pilha){
+// Pilha está cheia quando nao tiver mais memória disponível
+int cheia(Node **pilha){ 
 
 	if ((*pilha) == NULL)
 		return 1;
@@ -51,12 +52,13 @@ int cheia(Node **pilha){
 	return 0;
 }
 
+// Insere no Final
 int empilha(Node *pilha, int valor){
 
 	if (pilha == NULL) // Pilha nao existe
 		return 0;
 
-	if (cheia(pilha)) // Pilha cheia
+	if (cheia(&pilha)) // Pilha esta cheia
 		return 0;
 
 	Node *novo = (Node*)malloc(sizeof(Node));
@@ -75,7 +77,7 @@ int empilha(Node *pilha, int valor){
 
 	// Se não estiver vazia, insira na última posicao
 	
-	Node *ultimo = inicio->proximo;
+	Node *ultimo = pilha->proximo;
 
 	while (ultimo->proximo != NULL)
 		ultimo = ultimo->proximo;
@@ -84,7 +86,91 @@ int empilha(Node *pilha, int valor){
 
 }
 
+// retiro do final
+char desempilha(Node *pilha){
+
+	if (pilha == NULL) // Pilha nao existe
+		return 0;
+
+	if (vazia(pilha))
+		return 0;
+
+	Node *ultimo = pilha->proximo;
+	Node *penultimo = ultimo;
+
+	while(ultimo->proximo != NULL){
+		penultimo = ultimo;
+		ultimo = ultimo->proximo;
+	}
+
+	penultimo->proximo = NULL;
+
+	int valor = ultimo->conteudo;
+
+	free(ultimo);
+	return valor;
+}
+
+int tamanhoVetor(int vet[]){
+
+	int tamanho = sizeof(*vet) / sizeof(int);
+
+	return tamanho;
+}
+
+
+Node* ordenarDecrescente(int A[], int B[]){
+
+	Node *p1 = criarPilha(); // Pilha 1
+	Node *p2 = criarPilha(); // Pilha 2
+	
+	if (vazia(p1) || vazia(p2))
+		return 0;
+
+	Node *p3 = criarPilha(); // Pilha 3
+
+	int i = 0;
+	while (i < tamanhoVetor(A)){ // Empilhando valores do vetor A na Pilha 1
+		empilha(p1, A[i]);
+		i ++;
+	}
+
+	i = 0;
+	while (i < tamanhoVetor(B)){ // Empilhando valores do vetor B na Pilha 2
+		empilha(p2, B[i]);
+		i++;
+	}
+
+	int tamanho = tamanhoVetor(A) + tamanhoVetor(B); 
+
+	p3 = (Node*)realloc(p3, tamanho * sizeof(Node));
+
+	int x, y;
+
+	i = 0;
+	while (i < tamanho){
+
+		x = desempilha(p1);
+		y = desempilha(p2);
+
+		if (x > y)
+			p3[i].conteudo = x;
+			
+		else
+			p3[i].conteudo = y;
+
+		i++;
+	}
+
+	return p3;
+}
+
 void main(){
+
+	int num1[] = {1, 2, 3};
+	int num2[] = {4,5,6};
+
+	Node *pilha = ordenarDecrescente(num1, num2);
 
 
 }
