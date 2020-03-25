9f2caa57a738a632e057adc8398099ee35a876d7
Reinan de Souza <reinandesouza01@gmail.com>
2020-03-25 11:07:43 -0300

    Questao Completa

--- Lista 2/1.c
+++ Lista 2/1.c
@@ -1,8 +1,5 @@
 /*
 
-Checklist
-
-- Criacao de uma fila circular, usando lista encadeada. - completo
 - Fila 1 responsável pelas posicoes (0 - 9)
 - Fila 2 responsável pelas posicoes (10 - 19)
 
@@ -13,174 +10,203 @@ Obs: Teria sido melhor usar uma fila estática, mas ainda não sei tornar ela ci
 #include <stdio.h>
 #include <stdlib.h>
 
-struct no{
+typedef struct no{
+    int conteudo;
+    struct no *ptrToInicio;
+} No;
 
-	int conteudo;
-	struct no *proximo;
-};
+void inicializar(No vet[]);
+void incluirNaLista(No vetor[], int *pt1, int *pt2);
+int pesquisarNaLista(No vetor[], int pesquisar);
+void shift(No vetor[], int pos);
+void removerDaLista(No vetor[], int *pt1, int *pt2);
 
-typedef struct no No;
+int main(){
+    No vet[20];
+    int i, cont1 = 0, cont2 = 10;
+    int *c1, *c2;
 
-No* criaFila();
-int insereFinal(No **fila, int valor);
-int removeInicio(No **fila);
-int removeValor(No **fila, int valor);
-void liberaFila(No** fila);
-void imprimefila(No **fila);
+    c1 = &cont1;
+    c2 = &cont2;
 
-void main(){
-	
-	
+    inicializar(vet);
+
+    for(i=0; i<20;i++)
+        incluirNaLista(vet,c1,c2);
+
+    printf("impressao\n\n");
+
+    for(i=0; i<20;i++)
+        printf("%d\n",vet[i].conteudo);
+
+    pesquisarNaLista(vet, 42);
+
+    removerDaLista(vet, c1,c2);
+
+    printf("impressao\n\n");
+
+    for(i=0; i<20;i++)
+        printf("%d\n",vet[i].conteudo);
+
+    return 0;
+} 
+
+void incluirNaLista(No vetor[], int *pt1, int *pt2){
+    int op;
+
+    printf("Inserir na lista 1 ou 2?\n");
+    scanf("%d",&op);
+
+    if(op == 1){
+        if(*pt1 > 9){
+            printf("Lista cheia\n");
+            exit(0);
+        }
+
+        if(*pt1 == 9){
+            printf("Insira o valor ");
+            scanf("%d",&vetor[*pt1].conteudo);
+            vetor[*pt1].ptrToInicio = &vetor[0];
+            *pt1+=1;
+        }
+
+        if(*pt1 < 9){
+            printf("Insira o valor ");
+            scanf("%d",&vetor[*pt1].conteudo);
+            vetor[*pt1].ptrToInicio = &vetor[*pt1+1];
+            *pt1+=1;
+        }  
+    }else{
+        getchar();
+        if(*pt2 > 19){
+            printf("Lista cheia\n");
+            exit(0);
+        }
+
+        if(*pt2 == 19){
+            printf("Insira o valor ");
+            scanf("%d",&vetor[*pt2].conteudo);
+            vetor[*pt2].ptrToInicio = &vetor[10];
+        }
+
+        if(*pt2 > 9 && *pt2 < 19){
+            printf("Insira o valor ");
+            scanf("%d",&vetor[*pt2].conteudo);
+            vetor[*pt2].ptrToInicio = &vetor[*pt2+1];
+            *pt2+=1;
+        }
+    }
 }
 
-No* criaFila(){
+int pesquisarNaLista(No vetor[], int pesquisar){
+    int op, encontrou = 0;
+    int i1 = 0, i2 = 10;
 
-	No *inicio = (No*)malloc(sizeof(No));
+    printf("Pesquisar o valor %d na lista 1 ou 2?\n", pesquisar);
+    scanf("%d",&op);
 
-	if (inicio == NULL)
-		return NULL;
+    printf("Buscando o valor: %d...\n",pesquisar);
 
-	return inicio;
+    if(op==1){
+        while(i1< 9 && encontrou == 0){
+            if(vetor[i1].conteudo == pesquisar){
+                encontrou = 1;
+                printf("Encontrou o valor %d na lista 1\n",pesquisar);
+                return i1;
+            }else{
+                i1++;
+            }
+        }
+    }else{
+        while(i2 < 19 && encontrou == 0){
+            if(vetor[i2].conteudo == pesquisar){
+                encontrou = 1;
+                printf("Encontrou o valor %d na lista 2\n",pesquisar);
+                return i2;
+            }else{
+                i2++;
+            }
+        }
+    }
+
+    if(encontrou == 0)
+        printf("Nao encontrou o valor\n");
+
+    return 0;
 }
 
-int insereFinal(No **fila, int valor){
-
-	if (fila == NULL) // nao existe fila
-		return 0;
-
-	No *novo = (No*)malloc(sizeof(No));
-
-	if (novo == NULL) // sem espaco na memoria
-		return 0;
-
-	novo->conteudo = valor;
-
-	if ((*fila) == NULL){ // fila vazia: insere inicio
-		
-		*fila = novo;
-		novo->proximo = novo; // já que ele é o único na fila, ele vai apontar para ele mesmo		
-	
-	}else{
-
-		No *aux = *fila;
-
-		while(aux->proximo != (*fila))
-			aux = aux->proximo;
-
-		aux->proximo = novo;
-		novo->proximo = *fila;
-	}
-
-	return 1;
+void shift(No vetor[], int pos){
+    int aux;
+    if(pos != 9 && pos != 19){
+        if(pos < 9){
+            aux = 9-pos;
+            while(aux>1){
+                vetor[pos].conteudo = vetor[pos+1].conteudo;
+                pos++;
+                aux-=1;
+            }
+        }else{
+            aux = 19-pos;
+            while(aux>0){
+                vetor[pos].conteudo = vetor[pos+1].conteudo;
+                pos++;
+                aux-=1;
+            }
+        }
+    }
 }
 
-int removeInicio(No **fila){
+void removerDaLista(No vetor[], int *pt1, int *pt2){
+    int op, pesquisar, encontrou = 0;
+    int i1 = 0, i2 = 10;
+    
+    printf("Remover da lista 1 ou 2?\n");
+    scanf("%d",&op);
 
-	if (fila == NULL)
-		return 0;
+    printf("Qual valor Remover?\n");
+    scanf("%d",&pesquisar);
 
-	if ((*fila) == (*fila)->proximo){ // fila fica vazia
 
-		free(*fila);
-		*fila = NULL;
+    if(op==1){
+        while(i1< 9 && encontrou == 0){
+            if(vetor[i1].conteudo == pesquisar){
+                encontrou = 1;
+                printf("Removendo o valor %d na lista 1\n",pesquisar);
+                shift(vetor, i1);
+                *pt1 -=1;
+            }else{
+                i1++;
+            }
+        }
+    }else{
+        while(i2 < 19 && encontrou == 0){
+            if(vetor[i2].conteudo == pesquisar){
+                encontrou = 1;
+                printf("Removendo o valor %d na lista 2\n",pesquisar);
+                shift(vetor, i2);
+                *pt2 -=1;
+            }else{
+                i2++;
+            }
+        }
+    }
 
-		return 1;
-	}
-
-	No *atual = *fila;
-
-	while(atual->proximo != (*fila)) // procura o ultimo
-		atual = atual->proximo;
-
-	No *novo = *fila;
-	atual->proximo = novo->proximo;
-	*fila = novo->proximo;
-
-	free(novo);
-
-	return 1;
+    if(encontrou == 0)
+        printf("Nao encontrou o valor\n");
 }
 
-int removeValor(No **fila, int valor){
+void inicializar(No vet[]){
+    int i;
 
-	if (fila == NULL || (*fila) == NULL)
-		return 0;
+    //ZERAR TODO VETOR PARA EVITAR LIXO, apontar para o proximo e no fim de cada fila apontar para o inicio
+    for(i=0; i<20;i++){
+        vet[i].conteudo = 0;
 
-	No *novo = *fila;
-
-	if (novo->conteudo == valor){ // remover do inicio 
-
-		if (novo == novo->proximo){ // fila fica vazia
-			free(novo);
-
-			*fila = NULL;
-
-			return 1;
-
-		}else{
-
-			No *ultimo = *fila;
-
-			while (ultimo->proximo != (*fila)) // procura o ultimo
-				ultimo = ultimo->proximo;
-
-			ultimo->proximo = (*fila)->proximo;
-			*fila = (*fila)->proximo;
-
-			free(novo);
-
-			return 1;
-		}
-	}
-
-	No *anterior = novo;
-	novo = novo->proximo;
-
-	while (novo != (*fila) && novo->conteudo != valor){
-
-		anterior = novo;
-		novo = novo->proximo;
-	}
-
-	if (novo == *fila) // nao encontrado
-		return 0;
-
-	anterior->proximo = novo->proximo;
-
-	free(novo);
-
-	return 1;
-}
-
-void liberaFila(No** fila){
-
-	if (fila != NULL && (*fila) != NULL){
-
-		No *aux, *no = *fila;
-
-		while ((*fila) != no->proximo){
-
-			aux = no;
-			no = no->proximo;
-
-			free(aux);
-		}
-
-		free(no);
-		free(fila);
-	}
-}
-
-void imprimefila(No **fila){
-
-	if (fila == NULL || (*fila) == NULL)
-		return;
-
-	No *atual = *fila;
-
-	do{
-		printf("-> %d ", atual->conteudo);
-		atual = atual->proximo;
-	
-	}while (atual != (*fila));
+        if(i==9)
+            vet[i].ptrToInicio = &vet[0];
+        else if(i==19)
+            vet[i].ptrToInicio = &vet[0];
+        else 
+            vet[i].ptrToInicio = &vet[i+1];
+    }
 }
