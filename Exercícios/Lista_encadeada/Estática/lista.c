eafb723499c0fbd41eb58962d8972760c0bce5cc
Reinan de Souza <reinandesouza01@gmail.com>
2020-04-07 08:36:24 -0300

    Funcao Inserir no Inicio da  Lista

--- Exercícios/Lista_encadeada/Estática/lista.c
+++ Exercícios/Lista_encadeada/Estática/lista.c
@@ -22,6 +22,7 @@ Lista* criarLista(){
 
 	return inicio;
 }
+
 int liberaLista(Lista* li){
 
 	if (li == NULL)
@@ -43,6 +44,24 @@ int inserirFinal(Lista* li, int valor){
 	return 1;
 }
 
+int inserirInicio(Lista* li, int valor){
+
+	if (li == NULL)
+		return 0;
+
+	int i;
+
+	//7
+
+	for (i = li->qtd-1; i >= 0; i--) 
+		li->dados[i+1] = li->dados[i];
+
+	li->dados[0] = valor;
+
+	li->qtd ++;
+	return 1;
+}
+
 void exibirLista(Lista* li){
 
 	if (li == NULL || li->qtd == 0)
@@ -53,13 +72,15 @@ void exibirLista(Lista* li){
 	for (i = 0; i < li->qtd; i++)
 		printf("-> %d", li->dados[i]);
 
-	Lista *li = criarLista(); 
-
-	inserirFinal(li, 7);
-	exibirLista(li);
+	printf("\n");
 }
 
 void main(){
 
-	
+	Lista *li = criarLista(); 
+
+	inserirFinal(li, 7);
+	exibirLista(li);
+	inserirInicio(li, 1);
+	exibirLista(li);
 } 
