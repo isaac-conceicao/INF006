35286af2092cd6937e32286fd6ce5eb20759fa4f
Reinan de Souza <reinandesouza01@gmail.com>
2020-04-07 08:18:52 -0300

    Criacao de uma lista estática

---
+++ Exercícios/Lista_encadeada/Estática/lista.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 5
+
+struct lista{
+
+	int qtd; // vai servir para se mover dentro do vetor
+	int dados[MAX];
+};
+
+typedef struct lista Lista;
+
+void main(){
+
+	
+} 
