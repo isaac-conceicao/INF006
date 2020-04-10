4d3c431291ad4c9a8c6f37878c8e6598ba8fba22
Reinan de Souza <reinandesouza01@gmail.com>
2020-04-10 08:33:01 -0300

    Criacao dos Menus

---
+++ Lista 2/1.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+
+struct fila{
+	int inicioFilaUm;
+	int finalFilaUm;
+	int inicioFilaDois;
+	int finalFilaDois;
+	int qtdFila1;
+	int qtdFila2;
+	int dados[20];
+};
+
+typedef struct fila Fila;
+
+Fila* criarFila(){
+
+	Fila *fi = (Fila*)malloc(sizeof(Fila));
+
+	if (fi == NULL)
+		return NULL;
+
+	fi->inicioFilaUm = 0;
+	fi->finalFilaUm = 0;
+	fi->inicioFilaDois = 0;
+	fi->finalFilaDois = 0;
+	fi->qtdFila1 = 0;
+	fi->qtdFila2 = 0;
+
+	return fi; 
+}
+
+int insereFila(Fila *fi, int valor, int escolha){
+
+	if (escolha == 1){
+
+	}
+}
+
+int menuPrincipal(){
+
+	int opcao;
+
+	printf("\n<---- Menu ---->\n\n");
+	printf("1- Cria Fila \n");
+	printf("2- Menu Fila 1\n");
+	printf("3- Menu Fila 2\n");
+	printf("4- Consulta Fila\n");
+	printf("5- Sair\n\n");
+
+	printf("Opcao: ");
+	scanf("%d", &opcao);
+
+	return opcao;
+}
+
+int menuFilaUm(){
+
+	int opcao;
+
+	printf("<---- Menu Fila 1 -----> \n\n");
+	printf("1- Insere na Fila \n");
+	printf("2- Remove da Fila");
+
+	printf("Opcao: ");
+	scanf("%d", &opcao);
+
+	return opcao;
+}
+
+int menuFilaDois(){
+
+	int opcao;
+
+	printf("<---- Menu Fila 2 -----> \n\n");
+	printf("1- Insere na Fila \n");
+	printf("2- Remove da Fila");
+
+	printf("Opcao: ");
+	scanf("%d", &opcao);
+
+	return opcao;
+}
+
+void menuAuxiliar(){
+	
+	int escolha, opcao, valor;
+
+	
+
+	do{
+
+		escolha = menuPrincipal()
+
+		switch(escolha){
+
+			case 1:{
+				Lista *fi = criarFila();
+				break;
+			}
+
+			case 2:{
+				
+				opcao = menuFilaUm();
+
+				if (opcao == 1){
+					printf("Digite o Valor a ser Inserido: ");
+					scanf("%d", &valor);
+
+					insereFila(fi, valor);
+				
+				}else if (opcao == 2){
+					printf("Digite o Valor a ser Removido: ");
+					scanf("%d", &valor);
+
+					removeFila(fi, valor);
+				
+				}else
+					printf("\nOpcao Invalida\n\n");
+
+				break;
+			}
+
+			case 3:{
+				
+				opcao = menuFilaUm();
+
+				if (opcao == 1){
+					printf("Digite o Valor a ser Inserido: ");
+					scanf("%d", &valor);
+
+					insereFila(fi, valor);
+				
+				}else if (opcao == 2){
+					printf("Digite o Valor a ser Removido: ");
+					scanf("%d", &valor);
+
+					removeFila(fi, valor);
+				
+				}else
+					printf("\nOpcao Invalida\n\n");
+
+				break;
+			}
+
+			case 4:{
+				printf("Digite o Valor a ser Consultado: ");
+				scanf("%d", &valor);
+
+				consultaFila(fi, valor);
+
+				break;
+			}
+
+			case 5:{
+
+				printf("\nObrigado por Usar, volte Sempre\n\n");
+				break;
+			}
+		}
+
+	}while (escolha != 5);
+	
+}
+
+void main(){
+
+	menuAuxiliar();
+
+}
